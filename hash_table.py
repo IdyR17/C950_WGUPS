6dfd38343819d957f14fd607e2b5e816951724f4
Jurhidy Rodriguez Tavarez <IdyR17@users.noreply.github.com>
2026-09-19 17:29:20 -0400

    Implemented and tested insert function that handles collission through chaining

--- hash_table.py
+++ hash_table.py
@@ -10,4 +10,19 @@ class HashTable:
 
     # Hash function to convert a key into an index
     def get_hash_index(self, key):
-        return key % self.size       
+        return key % self.size
+    #Function to insert a key and value (package object) into the corresponding bucket in the hash table
+    #If there is a collision, it will be handled through chaining by appending the new package to the list.
+    def insert(self, key, value):
+        bucket_index = self.get_hash_index(key)
+        bucket = self.table[bucket_index]
+
+        # Check if the key already exists in the bucket
+        for item in bucket:
+            if item[0]==key: 
+                item[1] = value #update the package if the key already exists.
+                return
+
+        # if the key doesn't exist,  add a new key value pair to the bucket. 
+        # This is where chaining happens if there's a collision.
+        bucket.append([key, value])
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from package import Package
 from hash_table import HashTable
 
-print("WUPS Routing Program")
+print("WGUPS Routing Program")
 
 package_test1= Package(
     1,
@@ -22,3 +22,6 @@ test_hash_table = HashTable()
 #Testing hash function to see if it returns the same bucket.
 print(test_hash_table.get_hash_index(2))
 print(test_hash_table.get_hash_index(22))
+test_hash_table.insert(2, "Package test for collision")
+test_hash_table.insert(22, "Another package test for collision")
+print(test_hash_table.table)
