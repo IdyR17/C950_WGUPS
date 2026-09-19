e81326b90a25691b438d45fa9fe663fc6a49c0ca
Jurhidy Rodriguez Tavarez <IdyR17@users.noreply.github.com>
2026-09-19 18:34:37 -0400

    Refactor and cleaned up most test data from main

--- main.py
+++ main.py
@@ -5,24 +5,6 @@ from hash_table import HashTable
 
 print("WGUPS Routing Program")
 
-package_test1= Package(
-    1,
-    "1 abc st",
-    "Bergenfield",
-    "NJ",
-    "07621",
-    "8:00 AM",
-    "7",
-    "Nothing"
-)
-
-print(package_test1)
-
-test_hash_table = HashTable()
-#Testing with a real package object
-test_hash_table.insert(package_test1.package_id, package_test1)
-print(test_hash_table.get_package(1))  # Should print the package_test1 details
-
 #Load package data from the csv file into the hash table
 
 def load_packages(csv_file, hash_table):
@@ -46,6 +28,7 @@ def load_packages(csv_file, hash_table):
             package = Package(package_id, address, city, state, zip_code, deadline, weight, special_notes)
             hash_table.insert(package_id, package)
 
-load_packages("packages.csv", test_hash_table)
+package_table = HashTable()  # Create a package hash table to store the Package objects
+load_packages("packages.csv", package_table)
 for i in range(1, 41):  # Print all packages from 1 to 40
-    print(test_hash_table.get_package(i))
+    print(package_table.get_package(i))
